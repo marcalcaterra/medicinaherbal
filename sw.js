f92907cd43161ae4bc931fe2668c3c2958398485
marcalcaterra <javierfbravin@gmail.com>
2026-08-30 20:28:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mar-app-v4";
+const CACHE = "mar-app-v11";
 self.addEventListener("install", (event) => { self.skipWaiting(); });
 self.addEventListener("activate", (event) => {
   event.waitUntil(
